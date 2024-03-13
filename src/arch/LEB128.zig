74276200f7888e5b6c59a1563678e766fff9a13a
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-03-13 13:05:50 +0100

    arch: add LEB128 wrapper for consistent usage

---
+++ src/arch/LEB128.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+pub const LEB128 = struct {
+    pub fn encode(writer: anytype, value: i64) void {
+        std.leb.writeILEB128(writer, value);
+    }
+
+    pub fn decode(reader: anytype) !i64 {
+        return std.leb.readILEB128(i64, reader);
+    }
+};
