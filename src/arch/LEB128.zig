5e9013caba03a70e06ffc155f4ac640a20ffdf05
Ludvig Gunne Lindström <ludvig.gunne.lindstrom.dev@proton.me>
2024-03-13 13:17:43 +0100

    remove LEB128 struct decl

--- src/arch/LEB128.zig
+++ src/arch/LEB128.zig
@@ -1,11 +1,9 @@
 const std = @import("std");
 
-pub const LEB128 = struct {
-    pub fn encode(writer: anytype, value: i64) void {
-        std.leb.writeILEB128(writer, value);
-    }
+pub fn encode(writer: anytype, value: i64) void {
+    std.leb.writeILEB128(writer, value);
+}
 
-    pub fn decode(reader: anytype) !i64 {
-        return std.leb.readILEB128(i64, reader);
-    }
-};
+pub fn decode(reader: anytype) !i64 {
+    return std.leb.readILEB128(i64, reader);
+}
