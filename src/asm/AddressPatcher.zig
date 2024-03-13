90a9110205472524aa60c858306ba9198853a869
Ludvig Gunne Lindström <ludvig.gunne.lindstrom.dev@proton.me>
2024-03-13 20:18:51 +0100

    asm: rework AddressPatcher

--- src/asm/AddressPatcher.zig
+++ src/asm/AddressPatcher.zig
@@ -1,3 +1,6 @@
+//!
+//! Used for patching references to labels / functions
+//!
 const Self = @This();
 const std = @import("std");
 const int = @import("arch").int;
@@ -5,87 +8,95 @@ const int = @import("arch").int;
 /// Holds the address of a symbol / label and a list of references to it
 const Entry = struct {
     address: ?u64 = null,
-    refs: std.ArrayList(*[8]u8),
+    refs: std.ArrayList(usize),
     // TODO: Source reference
 
-    pub fn init(allocator: std.mem.Allocator) Self {
+    pub fn init(allocator: std.mem.Allocator) Entry {
         return .{
-            .refs = std.ArrayList(*[8]u8).init(allocator),
+            .refs = std.ArrayList(usize).init(allocator),
         };
     }
 };
 
 /// Iterator over unresolved symbol names
 const UnresolvedIterator = struct {
-    iterator: std.StringArrayHashMap(Entry).Iterator,
+    iterator: std.StringHashMap(Entry).Iterator,
 
     pub fn next(self: *UnresolvedIterator) ?[]const u8 {
-        const entry = self.iterator.next() orelse return null;
-        while (entry.?.value.address != null) {
+        var entry = self.iterator.next() orelse return null;
+        while (entry.value_ptr.address != null) {
             entry = self.iterator.next() orelse return null;
         }
-        return entry.?.key;
+        return entry.key_ptr.*;
     }
 };
 
-entries: std.StringArrayHashMap(Entry),
+entries: std.StringHashMap(Entry),
 allocator: std.mem.Allocator,
 
 pub fn init(allocator: std.mem.Allocator) Self {
     return .{
-        .entries = std.StringArrayHashMap(Entry).init(allocator),
+        .entries = std.StringHashMap(Entry).init(allocator),
         .allocator = allocator,
     };
 }
 
 pub fn deinit(self: *Self) void {
+    self.reset();
     self.entries.deinit();
 }
 
 /// Either patches a reference or defers the patch until the referenced
 /// symbol is resolved.
-pub fn patch(self: *Self, name: []const u8, patch_location: *[8]u8) !void {
-    var entry_result = try self.entries.getOrPut(name);
-
+pub fn patch(
+    self: *Self,
+    name: []const u8,
+    patch_location: usize,
+    code: []u8,
+) !void {
     // Create the entry if it does not exist
-    if (!entry_result.found_existing) {
-        entry_result.value_ptr.* = Entry.init(self.allocator);
+    if (!self.entries.contains(name)) {
+        try self.entries.put(name, Entry.init(self.allocator));
     }
+    var entry = self.entries.getPtr(name).?;
 
-    var entry = entry_result.value_ptr.*;
     if (entry.address == null) {
         // The address of the referenced symbol has not been resolved yet
         // so we defer the patch
         try entry.refs.append(patch_location);
     } else {
         // Patch the reference
-        int.encodeAddress(patch_location, entry.address.?);
+        int.encodeAddress(@ptrCast(code[patch_location .. patch_location + 8]), entry.address.?);
     }
 }
 
 /// Resolves the address of a symbol so that previous and future patches
 /// can be completed.
 /// Returns an error if the symbol has already been resolved
-pub fn resolve(self: *Self, name: []const u8, address: u64) !void {
-    var entry_result = try self.entries.getOrPut(name);
-
-    if (!entry_result.found_existing) {
-        // Create the entry
-        entry_result.value_ptr.* = Entry.init(self.allocator);
-    } else {
-        // Throw error if the symbol has already been resolved
-        if (entry_result.value_ptr.address != null) {
-            return error.AlreadyResolved;
-        }
+pub fn resolve(
+    self: *Self,
+    name: []const u8,
+    address: u64,
+    code: []u8,
+) !void {
+    // Create the entry if it does not exist
+    if (!self.entries.contains(name)) {
+        try self.entries.put(name, Entry.init(self.allocator));
     }
-    var entry = entry_result.value_ptr;
+    var entry = self.entries.getPtr(name).?;
+
+    // Check if the symbol has already been resolved
+    if (entry.address != null) {
+        return error.AlreadyResolved;
+    }
     entry.address = address;
 
+    // Patch previous references
     for (entry.refs.items) |patch_location| {
-        int.encodeAddress(patch_location, address);
+        int.encodeAddress(@ptrCast(code[patch_location .. patch_location + 8]), address);
     }
 
-    entry.refs.deinit();
+    entry.refs.clearRetainingCapacity();
 }
 
 /// Returns an iterator over the unresolved symbol names
@@ -95,6 +106,9 @@ pub fn unresolvedIterator(self: *Self) UnresolvedIterator {
 
 /// Resets the patcher
 pub fn reset(self: *Self) void {
+    var iterator = self.entries.iterator();
+    while (iterator.next()) |entry| {
+        entry.value_ptr.refs.deinit();
+    }
     self.entries.clearRetainingCapacity();
-    self.unresolved = 0;
 }
