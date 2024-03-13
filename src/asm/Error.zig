21e5dbfcdda8e420010996b684d1badbe4b257be
Ludvig Gunne Lindström <ludvig.gunne.lindstrom.dev@proton.me>
2024-03-13 12:03:05 +0100

    add generic error struct

---
+++ src/asm/Error.zig
@@ -0,0 +1,40 @@
+//!
+//! Generic error type that can be shared between assembler and compiler
+//!
+const Self = @This();
+const SourceRef = @import("SourceRef.zig");
+
+/// The kind of error
+tag: enum {
+    /// Invalid character in input stream
+    invalid_character,
+    /// (Assembler) Word following keyword prefix is not a valid keyword
+    invalid_keyword,
+    /// (Assembler) Word is not a valid instruction
+    invalid_instruction,
+    /// (Assembler) Word following literal prefix is not a valid literal
+    invalid_literal,
+    /// (Assembler) String literal has no closing double quote
+    unterminated_string,
+    /// (Assembler) Operand is missing from multibyte instruction
+    missing_operand,
+    /// (Assembler) Operand is supplied for instruction that does not take operands
+    redundant_operand,
+    /// Unexpected token in input stream
+    unexpected_token,
+},
+/// Location in source
+where: []const u8,
+/// Optional error info
+extra: ?[]const u8 = null,
+
+/// Prints error with source location
+pub fn print(self: Self, source: []const u8, writer: anytype) !void {
+    const ref = try SourceRef.init(source, self.where);
+    try writer.print("Error on line {d}: {s}", .{ ref.line_num, @tagName(self.tag) });
+    if (self.extra) |extra| {
+        try writer.print(": {s}", .{extra});
+    }
+    try writer.print(".\n", .{});
+    try ref.print(writer);
+}
