78fc67a2d4366c8444144ed928396f02b08c8e22
Ludvig Gunne Lindström <ludvig.gunne.lindstrom.dev@proton.me>
2024-03-23 18:48:40 +0100

    asm: patching tests

--- src/asm/AsmTest.zig
+++ src/asm/AsmTest.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Asm = @import("Asm.zig");
 const Error = @import("Error.zig");
+const Instruction = @import("arch").instr.Instruction;
 
 const Tag = @TypeOf(@as(Error, undefined).tag);
 
@@ -107,4 +108,63 @@ test "success" {
     try asm_.assemble();
 
     try std.testing.expectEqual(@as(usize, 0), errors.items.len);
+}
+
+test "patching calls" {
+    const source =
+        \\-function "main"
+        \\-begin
+        \\call "other" #0
+        \\push %0      #1 (doing some other stuff to make it interesting)
+        \\pop          #2
+        \\load %0      #3
+        \\store %0     #4
+        \\-end
+        \\
+        \\-function "other"
+        \\-begin
+        \\push %0      #5
+        \\-end
+    ;
+
+    var errors = std.ArrayList(Error).init(std.testing.allocator);
+    defer errors.deinit();
+
+    var asm_ = Asm.init(source, std.testing.allocator, &errors);
+    defer asm_.deinit();
+
+    try asm_.assemble();
+    const code = asm_.getCode();
+
+    try std.testing.expectEqual(@as(usize, 0), errors.items.len);
+    try std.testing.expectEqual(@as(usize, 6), code.len);
+    try std.testing.expectEqual(Instruction.call, code[0].op);
+    try std.testing.expectEqual(@as(usize, 5), code[0].operand.location);
+}
+
+test "patching labels" {
+    const source =
+        \\-function "main"
+        \\-begin
+        \\jmp .label   #0
+        \\push %0      #1
+        \\pop          #2
+        \\.label
+        \\load %0      #3
+        \\-end
+    ;
+
+    var errors = std.ArrayList(Error).init(std.testing.allocator);
+    defer errors.deinit();
+
+    var asm_ = Asm.init(source, std.testing.allocator, &errors);
+    defer asm_.deinit();
+
+    try asm_.assemble();
+    const code = asm_.getCode();
+
+    try std.testing.expectEqual(@as(usize, 0), errors.items.len);
+    try std.testing.expectEqual(@as(usize, 4), code.len);
+    try std.testing.expectEqual(Instruction.jmp, code[0].op);
+    try std.testing.expectEqual(@as(usize, 3), code[0].operand.location);
 }
