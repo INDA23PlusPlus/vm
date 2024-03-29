0cee4a2013056cb24169b1955a5abbeb19e19eab
JonathanHallstrom <lmj.hallstrom@gmail.com>
2024-03-29 09:37:59 +0100

    vm: use VMProgram in main

--- src/vm/main.zig
+++ src/vm/main.zig
@@ -1,15 +1,16 @@
 const std = @import("std");
 const Interpreter = @import("Interpreter.zig");
 const VMInstruction = @import("VMInstruction.zig");
+const VMProgram = @import("VMProgram.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     std.debug.print("program result: {}\n", .{try Interpreter.run(
-        &.{
+        &VMProgram.init(&.{
             VMInstruction.push(2),
             VMInstruction.push(1),
             VMInstruction.greater(),
-        },
+        }, 0),
         gpa.allocator(),
         std.io.getStdOut().writer(),
         true,
