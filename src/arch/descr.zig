d71b00dcd424f8e81fa33f031507d271acd2dfcb
Ludvig Gunne Lindström <ludvig.gunne.lindstrom.dev@proton.me>
2024-04-29 21:25:35 +0200

    arch: make hover info headers smaller

--- src/arch/descr.zig
+++ src/arch/descr.zig
@@ -11,7 +11,7 @@ pub const text = blk: {
     const entries = [_]Entry{
         .{
             .add,
-            \\# Addition
+            \\## Addition
             \\
             \\Pops the top two elements from the stack and attempts to perform addition on them.
             \\Pushes the result to the stack on success.
@@ -20,7 +20,7 @@ pub const text = blk: {
         },
         .{
             .sub,
-            \\# Subtraction
+            \\## Subtraction
             \\
             \\Pops the top two elements from the stack and attempts to perform subtraction on them.
             \\Pushes the result to the stack on success.
@@ -31,7 +31,7 @@ pub const text = blk: {
         },
         .{
             .mul,
-            \\# Multiplication
+            \\## Multiplication
             \\
             \\Pops the top two elements from the stack and attempts to perform multiplication on them.
             \\Pushes the result to the stack on success.
@@ -40,7 +40,7 @@ pub const text = blk: {
         },
         .{
             .div,
-            \\# Division
+            \\## Division
             \\
             \\Pops the top two elements from the stack and attempts to perform division on them.
             \\Pushes the result to the stack on success.
@@ -51,7 +51,7 @@ pub const text = blk: {
         },
         .{
             .mod,
-            \\# Modulus
+            \\## Modulus
             \\
             \\Pops the top two elements from the stack and attempts to perform division on them.
             \\Pushes the remainder to the stack on success.
@@ -62,7 +62,7 @@ pub const text = blk: {
         },
         .{
             .cmp_lt,
-            \\# Less than
+            \\## Less than
             \\
             \\Pops the top two elements from the stack and compares them.
             \\Pushes 1 if the second element to be popped is less than the
@@ -72,7 +72,7 @@ pub const text = blk: {
         },
         .{
             .cmp_gt,
-            \\# Greater than
+            \\## Greater than
             \\
             \\Pops the top two elements from the stack and compares them.
             \\Pushes 1 if the second element to be popped is greater than the
@@ -82,7 +82,7 @@ pub const text = blk: {
         },
         .{
             .cmp_le,
-            \\# Less than or equal
+            \\## Less than or equal
             \\
             \\Pops the top two elements from the stack and compares them.
             \\Pushes 1 if the second element to be popped is less than or equal to the
@@ -92,7 +92,7 @@ pub const text = blk: {
         },
         .{
             .cmp_ge,
-            \\# Greater than or equal
+            \\## Greater than or equal
             \\
             \\Pops the top two elements from the stack and compares them.
             \\Pushes 1 if the second element to be popped is greater than or equal to the
@@ -102,7 +102,7 @@ pub const text = blk: {
         },
         .{
             .cmp_eq,
-            \\# Equal
+            \\## Equal
             \\
             \\Pops the top two elements from the stack and compares them for equality.
             \\Pushes 1 if they are equal, 0 otherwise.
@@ -111,7 +111,7 @@ pub const text = blk: {
         },
         .{
             .cmp_ne,
-            \\# Equal
+            \\## Equal
             \\
             \\Pops the top two elements from the stack and compares them for equality.
             \\Pushes 1 if they are not equal, 0 otherwise.
@@ -120,7 +120,7 @@ pub const text = blk: {
         },
         .{
             .jmp,
-            \\# Unconditional jump
+            \\## Unconditional jump
             \\
             \\Jumps unconditionally to the label supplied as its operand.
             \\
@@ -128,7 +128,7 @@ pub const text = blk: {
         },
         .{
             .jmpnz,
-            \\# Conditional jump
+            \\## Conditional jump
             \\
             \\Pops the top element the stack, jumps to the label supplied as operand if and
             \\only if the popped element is non-zero.
@@ -137,7 +137,7 @@ pub const text = blk: {
         },
         .{
             .push,
-            \\# Push integer
+            \\## Push integer
             \\
             \\Push the integer literal supplied as its operand to the stack.
             \\
@@ -145,7 +145,7 @@ pub const text = blk: {
         },
         .{
             .pushf,
-            \\# Push float
+            \\## Push float
             \\
             \\Push the float literal supplied as its operand to the stack.
             \\
@@ -153,7 +153,7 @@ pub const text = blk: {
         },
         .{
             .pop,
-            \\# Pop
+            \\## Pop
             \\
             \\Pop the top element of the stack and discard it.
             \\
@@ -161,7 +161,7 @@ pub const text = blk: {
         },
         .{
             .dup,
-            \\# Duplicate
+            \\## Duplicate
             \\
             \\Duplicates the top element of the stack. Does not copy reference types.
             \\
@@ -169,7 +169,7 @@ pub const text = blk: {
         },
         .{
             .load,
-            \\# Load local variable/parameter
+            \\## Load local variable/parameter
             \\
             \\Loads the local variable/parameter at the offset supplied as its operand,
             \\and pushes it to the stack.
@@ -179,7 +179,7 @@ pub const text = blk: {
         },
         .{
             .store,
-            \\# Store local variable/parameter
+            \\## Store local variable/parameter
             \\
             \\Stores a value to the local variable/parameter at the offset supplied as its operand.
             \\The offset is relative to the base pointer. The value is popped from the stack before
@@ -189,7 +189,7 @@ pub const text = blk: {
         },
         .{
             .syscall,
-            \\# Perform syscall
+            \\## Perform syscall
             \\
             \\Performs a syscall with number supplied as its operand.
             \\Currently supported syscalls are:
@@ -199,7 +199,7 @@ pub const text = blk: {
         },
         .{
             .call,
-            \\# Call function
+            \\## Call function
             \\
             \\Calls the function with identifier supplied as its operand.
             \\
@@ -207,7 +207,7 @@ pub const text = blk: {
         },
         .{
             .ret,
-            \\# Return from function
+            \\## Return from function
             \\
             \\Pops the return value from stack and returns from the current function.
             \\Any passed arguments are popped from the stack and the return value is
@@ -217,7 +217,7 @@ pub const text = blk: {
         },
         .{
             .stack_alloc,
-            \\# Stack allocation
+            \\## Stack allocation
             \\
             \\Allocates N slots on the stack, where N is the integer literal supplied as operand,
             \\and initializes them all as Unit.
@@ -226,7 +226,7 @@ pub const text = blk: {
         },
         .{
             .struct_alloc,
-            \\# Struct creation
+            \\## Struct creation
             \\
             \\Creates an empty struct and pushes a reference to it to the stack.
             \\
@@ -234,7 +234,7 @@ pub const text = blk: {
         },
         .{
             .struct_load,
-            \\# Load struct field
+            \\## Load struct field
             \\
             \\Pops a struct reference from the stack. Tries to access the field with identifier
             \\supplied as operand and push it to the stack. If the field has not yet been initialized,
@@ -244,7 +244,7 @@ pub const text = blk: {
         },
         .{
             .struct_store,
-            \\# Store struct field
+            \\## Store struct field
             \\
             \\Pops a struct reference and a value from the stack. Stores the value at field with identifier
             \\supplied as operand in popped struct.
@@ -253,7 +253,7 @@ pub const text = blk: {
         },
         .{
             .list_alloc,
-            \\# List creation
+            \\## List creation
             \\
             \\Creates an empty list and pushes a reference to it to the stack.
             \\
@@ -261,7 +261,7 @@ pub const text = blk: {
         },
         .{
             .list_load,
-            \\# Load list element
+            \\## Load list element
             \\
             \\ TODO !!!
             \\
@@ -269,7 +269,7 @@ pub const text = blk: {
         },
         .{
             .list_store,
-            \\# Store list element
+            \\## Store list element
             \\
             \\ TODO !!!
             \\
