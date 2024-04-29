d4763327dc2eb5ec0161002d52225a66b6a40f07
Ludvig Gunne Lindström <ludvig.gunne.lindstrom.dev@proton.me>
2024-04-29 20:49:52 +0200

    arch: add in depth opcode descriptions

--- src/arch/descr.zig
+++ src/arch/descr.zig
@@ -11,147 +11,268 @@ pub const text = blk: {
     const entries = [_]Entry{
         .{
             .add,
-            \\ [a, b] -> [a + b]
+            \\# Addition
+            \\
+            \\Pops the top two elements from the stack and attempts to perform addition on them.
+            \\Pushes the result to the stack on success.
+            \\
             ,
         },
         .{
             .sub,
-            \\ [a, b] -> [a - b]
+            \\# Subtraction
+            \\
+            \\Pops the top two elements from the stack and attempts to perform subtraction on them.
+            \\Pushes the result to the stack on success.
+            \\The subtraction is performed with it's terms in the same order as they are pushed
+            \\to the stack.
+            \\
             ,
         },
         .{
             .mul,
-            \\ [a, b] -> [a * b]
+            \\# Multiplication
+            \\
+            \\Pops the top two elements from the stack and attempts to perform multiplication on them.
+            \\Pushes the result to the stack on success.
+            \\
             ,
         },
         .{
             .div,
-            \\ [a, b] -> [a / b] (rounds toward zero)
+            \\# Division
+            \\
+            \\Pops the top two elements from the stack and attempts to perform division on them.
+            \\Pushes the result to the stack on success.
+            \\The division is performed with it's terms in the same order as they are pushed
+            \\to the stack.
+            \\
             ,
         },
         .{
             .mod,
-            \\ [a, b] -> [a % b] TODO: decide how to handle modulo/remainder
+            \\# Modulus
+            \\
+            \\Pops the top two elements from the stack and attempts to perform division on them.
+            \\Pushes the remainder to the stack on success.
+            \\The division is performed with it's terms in the same order as they are pushed
+            \\to the stack.
+            \\
             ,
         },
         .{
             .cmp_lt,
-            \\ [a, b] -> [res] where res = a < b
+            \\# Less than
+            \\
+            \\Pops the top two elements from the stack and compares them.
+            \\Pushes 1 if the second element to be popped is less than the
+            \\first, pushes 0 otherwise.
+            \\
             ,
         },
         .{
             .cmp_gt,
-            \\ [a, b] -> [res] where res = a > b
+            \\# Greater than
+            \\
+            \\Pops the top two elements from the stack and compares them.
+            \\Pushes 1 if the second element to be popped is greater than the
+            \\first, pushes 0 otherwise.
+            \\
             ,
         },
         .{
             .cmp_le,
-            \\ [a, b] -> [res] where res = a <= b
+            \\# Less than or equal
+            \\
+            \\Pops the top two elements from the stack and compares them.
+            \\Pushes 1 if the second element to be popped is less than or equal to the
+            \\first, pushes 0 otherwise.
+            \\
             ,
         },
         .{
             .cmp_ge,
-            \\ [a, b] -> [res] where res = a <= b
+            \\# Greater than or equal
+            \\
+            \\Pops the top two elements from the stack and compares them.
+            \\Pushes 1 if the second element to be popped is greater than or equal to the
+            \\first, pushes 0 otherwise.
+            \\
             ,
         },
         .{
             .cmp_eq,
-            \\ [a, b] -> [res] where res = a == b
+            \\# Equal
+            \\
+            \\Pops the top two elements from the stack and compares them for equality.
+            \\Pushes 1 if they are equal, 0 otherwise.
+            \\
             ,
         },
         .{
             .cmp_ne,
-            \\ [a, b] -> [res] where res = a != b
+            \\# Equal
+            \\
+            \\Pops the top two elements from the stack and compares them for equality.
+            \\Pushes 1 if they are not equal, 0 otherwise.
+            \\
             ,
         },
         .{
             .jmp,
-            \\ OP .destination [] -> [] control flow continues at .destination
+            \\# Unconditional jump
+            \\
+            \\Jumps unconditionally to the label supplied as its operand.
+            \\
             ,
         },
         .{
             .jmpnz,
-            \\ OP .destination [a] -> [] control flow continues at .destination if a != 0
+            \\# Conditional jump
+            \\
+            \\Pops the top element the stack, jumps to the label supplied as operand if and
+            \\only if the popped element is non-zero.
+            \\
             ,
         },
         .{
             .push,
-            \\ OP %value [] -> [value]
+            \\# Push integer
+            \\
+            \\Push the integer literal supplied as its operand to the stack.
+            \\
             ,
         },
         .{
             .pushf,
-            \\ OP @value [] -> [value]
+            \\# Push float
+            \\
+            \\Push the float literal supplied as its operand to the stack.
+            \\
             ,
         },
         .{
             .pop,
-            \\ [a] -> []
+            \\# Pop
+            \\
+            \\Pop the top element of the stack and discard it.
+            \\
             ,
         },
         .{
             .dup,
-            \\ [a] -> [a, a]
+            \\# Duplicate
+            \\
+            \\Duplicates the top element of the stack. Does not copy reference types.
+            \\
             ,
         },
         .{
             .load,
-            \\ OP %i [] -> [value] where value = stack[BP + i]
+            \\# Load local variable/parameter
+            \\
+            \\Loads the local variable/parameter at the offset supplied as its operand,
+            \\and pushes it to the stack.
+            \\The offset is relative to the base pointer.
+            \\
             ,
         },
         .{
             .store,
-            \\ OP %i [value] -> [] sets stack[BP + i] = value
+            \\# Store local variable/parameter
+            \\
+            \\Stores a value to the local variable/parameter at the offset supplied as its operand.
+            \\The offset is relative to the base pointer. The value is popped from the stack before
+            \\storing.
+            \\
             ,
         },
         .{
             .syscall,
-            \\ [args...] -> [ret...] TODO: keep or remove
+            \\# Perform syscall
+            \\
+            \\Performs a syscall with number supplied as its operand.
+            \\Currently supported syscalls are:
+            \\* %0: pop and print
+            \\
             ,
         },
         .{
             .call,
-            \\ OP .f [param 0, ..., param N - 1, N] -> [param 0, ..., param N - 1, N, BP, return_address]
+            \\# Call function
+            \\
+            \\Calls the function with identifier supplied as its operand.
+            \\
             ,
         },
         .{
             .ret,
-            \\ [param 0, ..., param N - 1, N, BP, ..., return_address, return_value] -> [return_value]
+            \\# Return from function
+            \\
+            \\Pops the return value from stack and returns from the current function.
+            \\Any passed arguments are popped from the stack and the return value is
+            \\pushed.
+            \\
             ,
         },
         .{
             .stack_alloc,
-            \\ Allocates N unit object on the stack
+            \\# Stack allocation
+            \\
+            \\Allocates N slots on the stack, where N is the integer literal supplied as operand,
+            \\and initializes them all as Unit.
+            \\
             ,
         },
         .{
             .struct_alloc,
-            \\[] -> [s] where s is a reference to the newly allocated struct
+            \\# Struct creation
+            \\
+            \\Creates an empty struct and pushes a reference to it to the stack.
+            \\
             ,
         },
         .{
             .struct_load,
-            \\OP %f [s] -> [v] where v = s.f
+            \\# Load struct field
+            \\
+            \\Pops a struct reference from the stack. Tries to access the field with identifier
+            \\supplied as operand and push it to the stack. If the field has not yet been initialized,
+            \\pushes a Unit object.
+            \\
             ,
         },
         .{
             .struct_store,
-            \\OP %f [s, v] -> [] sets s.f = v
+            \\# Store struct field
+            \\
+            \\Pops a struct reference and a value from the stack. Stores the value at field with identifier
+            \\supplied as operand in popped struct.
+            \\
             ,
         },
         .{
             .list_alloc,
-            \\ [] -> [l] where l is a reference to the newly allocated list
+            \\# List creation
+            \\
+            \\Creates an empty list and pushes a reference to it to the stack.
+            \\
             ,
         },
         .{
             .list_load,
-            \\ [l, i] -> [l, v] where v = l[i]
+            \\# Load list element
+            \\
+            \\ TODO !!!
+            \\
             ,
         },
         .{
             .list_store,
-            \\ [l, i, v] -> [l] sets l[i] = v
+            \\# Store list element
+            \\
+            \\ TODO !!!
+            \\
             ,
         },
     };
