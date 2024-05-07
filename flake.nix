12d203bde58c80cb4a31127b7d6999853685bee9
ludviggl <ludvig.gunne.lindstrom.dev@proton.me>
2024-05-07 19:10:02 +0200

    flake.nix: clean up

--- flake.nix
+++ flake.nix
@@ -13,53 +13,52 @@
         pkgs = import inputs.nixpkgs {inherit system;};
         inherit (inputs.gitignore.lib) gitignoreSource;
       in rec {
-        devShells.default = devShells.develop;
-        packages.default = packages.vemod;
 
-        devShells.develop = pkgs.mkShell { nativeBuildInputs = with pkgs; [ zig_0_12 zls ]; };
-        devShells.use = pkgs.mkShell { nativeBuildInputs = with packages; [ vemod vmdls ]; };
-
-        packages.vemod = pkgs.stdenvNoCC.mkDerivation {
-          pname = "vemod";
-          version = "master";
-          src = gitignoreSource ./.;
-          nativeBuildInputs = with pkgs; [ zig ];
-          dontConfigure = true;
-          dontInstall = true;
-          doCheck = true;
-          buildPhase = ''
-            export XDG_CACHE_HOME=$(mktemp -d)
-            zig build vemod --prefix $out -Doptimize=ReleaseFast
-            rm -rf $XDG_CACHE_HOME
-          '';
-          checkPhase = ''
-            export XDG_CACHE_HOME=$(mktemp -d)
-            zig fmt --check .
-            zig build test
-            rm -rf $XDG_CACHE_HOME
-          '';
+        devShells = rec {
+          default = develop;
+          develop = pkgs.mkShell { nativeBuildInputs = with pkgs; [ zig_0_12 zls ]; };
+          use = pkgs.mkShell { nativeBuildInputs = with packages; [ vemod vmdls ]; };
         };
 
-        packages.vmdls = pkgs.stdenvNoCC.mkDerivation {
-          pname = "vmdls";
-          version = "master";
-          src = gitignoreSource ./.;
-          nativeBuildInputs = with pkgs; [ zig ];
-          dontConfigure = true;
-          dontInstall = true;
-          doCheck = true;
-          buildPhase = ''
-            export XDG_CACHE_HOME=$(mktemp -d)
-            zig build vmdls --prefix $out -Doptimize=ReleaseFast
-            rm -rf $XDG_CACHE_HOME
-          '';
-          checkPhase = ''
-            export XDG_CACHE_HOME=$(mktemp -d)
-            zig fmt --check .
-            zig build test
-            rm -rf $XDG_CACHE_HOME
-          '';
-        };
+        packages = 
+          let
+            commonDrvAttrs = {
+              src = gitignoreSource ./.;
+              nativeBuildInputs = with pkgs; [ zig ];
+              dontConfigure = true;
+              dontInstall = true;
+              doCheck = true;
+              checkPhase = ''
+                export XDG_CACHE_HOME=$(mktemp -d)
+                zig fmt --check .
+                zig build test
+                rm -rf $XDG_CACHE_HOME
+              '';
+            };
+          in
+          rec {
+            default = vemod;
+
+            vemod = pkgs.stdenvNoCC.mkDerivation (commonDrvAttrs // {
+              pname = "vemod";
+              version = "master";
+              buildPhase = ''
+                export XDG_CACHE_HOME=$(mktemp -d)
+                zig build vemod --prefix $out -Doptimize=ReleaseFast
+                rm -rf $XDG_CACHE_HOME
+              '';
+            });
+
+            vmdls = pkgs.stdenvNoCC.mkDerivation (commonDrvAttrs // {
+              pname = "vmdls";
+              version = "master";
+              buildPhase = ''
+                export XDG_CACHE_HOME=$(mktemp -d)
+                zig build vmdls --prefix $out -Doptimize=ReleaseFast
+                rm -rf $XDG_CACHE_HOME
+              '';
+            });
+          };
       }
     );
 }
