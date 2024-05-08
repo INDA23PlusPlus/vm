ac8f1a910279125a5abfae7df88bcfa239e041a7
ludviggl <ludvig.gunne.lindstrom.dev@proton.me>
2024-05-08 14:52:47 +0200

    flake.nix: add package version without checks

--- flake.nix
+++ flake.nix
@@ -17,18 +17,17 @@
         devShells = rec {
           default = develop;
           develop = pkgs.mkShell { nativeBuildInputs = with pkgs; [ zig_0_12 zls ]; };
-          use = pkgs.mkShell { nativeBuildInputs = with packages; [ vemod vmdls ]; };
+          use = pkgs.mkShell { nativeBuildInputs = with packages; [ vemod-no-check vmdls-no-check ]; };
         };
 
         packages = 
           let
-            mkVemodPkg = { pname, version ? "master" }: pkgs.stdenvNoCC.mkDerivation {
-              inherit pname version;
+            mkVemodPkg = { pname, version ? "master", doCheck ? true }: pkgs.stdenvNoCC.mkDerivation {
+              inherit pname version doCheck;
               src = gitignoreSource ./.;
               nativeBuildInputs = with pkgs; [ zig_0_12 ];
               dontConfigure = true;
               dontInstall = true;
-              doCheck = true;
               buildPhase = ''
                 export XDG_CACHE_HOME=$(mktemp -d)
                 zig build ${pname} --prefix $out -Doptimize=ReleaseFast
@@ -46,6 +45,8 @@
             default = vemod;
             vemod = mkVemodPkg { pname = "vemod"; };
             vmdls = mkVemodPkg { pname = "vmdls"; };
+            vemod-no-check = mkVemodPkg { pname = "vemod"; doCheck = false; };
+            vmdls-no-check = mkVemodPkg { pname = "vmdls"; doCheck = false; };
           };
       }
     );
