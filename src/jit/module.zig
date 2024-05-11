97f0ef3f6f8fa19acd01be2f792bf95c856b5dba
glank <glankk@users.noreply.github.com>
2024-05-12 00:41:42 +0200

    jit: update module.zig

--- src/jit/module.zig
+++ src/jit/module.zig
@@ -1,6 +1,9 @@
+pub const Function = @import("Function.zig");
 pub const Jit = @import("Jit.zig");
 
 test {
     _ = @import("as.zig");
+    _ = @import("exec_context.zig");
+    _ = @import("Function.zig");
     _ = @import("Jit.zig");
 }
