490ef5667878c5f7a36fb7971722d6a90c86011c
ludviggl <ludvig.gunne.lindstrom.dev@proton.me>
2024-05-11 11:08:00 +0200

    flake.nix: add debug packages and devshell

--- flake.nix
+++ flake.nix
@@ -18,33 +18,37 @@
           default = develop;
           develop = pkgs.mkShell { nativeBuildInputs = with pkgs; [ zig_0_12 zls ]; };
           use = pkgs.mkShell { nativeBuildInputs = with packages; [ vemod-no-check vmdls-no-check ]; };
+          use-debug = pkgs.mkShell { nativeBuildInputs = with packages; [ vemod-debug vmdls-debug ]; };
         };
 
         packages = 
           let
-            mkVemodPkg = { pname, version ? "master", doCheck ? true }: pkgs.stdenvNoCC.mkDerivation {
-              inherit pname version doCheck;
-              src = gitignoreSource ./.;
-              nativeBuildInputs = with pkgs; [ zig_0_12 ];
-              dontConfigure = true;
-              dontInstall = true;
-              buildPhase = ''
-                export XDG_CACHE_HOME=$(mktemp -d)
-                zig build ${pname} --prefix $out -Doptimize=ReleaseFast
-                rm -rf $XDG_CACHE_HOME
-              '';
-              checkPhase = ''
-                export XDG_CACHE_HOME=$(mktemp -d)
-                zig fmt --check .
-                zig build test
-                rm -rf $XDG_CACHE_HOME
-              '';
-            };
+            mkVemodPkg = { pname, version ? "master", doCheck ? true, optimize ? "ReleaseFast" }: 
+              pkgs.stdenvNoCC.mkDerivation {
+                inherit pname version doCheck;
+                src = gitignoreSource ./.;
+                nativeBuildInputs = with pkgs; [ zig_0_12 ];
+                dontConfigure = true;
+                dontInstall = true;
+                buildPhase = ''
+                  export XDG_CACHE_HOME=$(mktemp -d)
+                  zig build ${pname} --prefix $out -Doptimize=${optimize}
+                  rm -rf $XDG_CACHE_HOME
+                '';
+                checkPhase = ''
+                  export XDG_CACHE_HOME=$(mktemp -d)
+                  zig fmt --check .
+                  zig build test
+                  rm -rf $XDG_CACHE_HOME
+                '';
+              };
           in
           rec {
             default = vemod;
             vemod = mkVemodPkg { pname = "vemod"; };
             vmdls = mkVemodPkg { pname = "vmdls"; };
+            vemod-debug = mkVemodPkg { pname = "vemod"; doCheck = false; optimize = "Debug"; };
+            vmdls-debug = mkVemodPkg { pname = "vmdls"; doCheck = false; optimize = "Debug"; };
             vemod-no-check = mkVemodPkg { pname = "vemod"; doCheck = false; };
             vmdls-no-check = mkVemodPkg { pname = "vmdls"; doCheck = false; };
           };
