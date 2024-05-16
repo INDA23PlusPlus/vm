b5669341f39f94e5c66c43f0a56f85ef5dc5f7e9
ludviggl <ludvig.gunne.lindstrom.dev@proton.me>
2024-05-16 21:54:14 +0200

    vscode: add transpile command

--- editor/vscode/src/extension.ts
+++ editor/vscode/src/extension.ts
@@ -1,7 +1,12 @@
 import * as vscode from 'vscode';
 import * as vmdls from './vmdls';
+import * as child_process from 'child_process';
 
 const vemodLanguageIds = [ "vemod", "blue" ];
+let outputChannel: vscode.OutputChannel;
+
+// TODO: replace with configuration
+const vemodCommand = "vemod";
 
 export async function activate(context: vscode.ExtensionContext) {
     await vmdls.activate(context);
@@ -9,8 +14,11 @@ export async function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(
         vscode.commands.registerCommand('vemod.runFile', () => runVemodOnActiveDocument("")),
         vscode.commands.registerCommand('vemod.runFileForceJit', () => runVemodOnActiveDocument("-j full")),
+        vscode.commands.registerCommand('vemod.transpileFile', () => transpileFile()),
         // TODO: transpile, compile, install
     );
+
+    outputChannel = vscode.window.createOutputChannel("VeMod");
 }
 
 function getVemodTerminal() {
@@ -22,47 +30,96 @@ function getVemodTerminal() {
     return vscode.window.createTerminal({ name: name });
 }
 
-// Returns the filename of the active document, or null if the active document is
-// not VeMod or Blue source.
-function getActiveFilename() {
-
-    const activeEditor = vscode.window.activeTextEditor;
-    if (!activeEditor) {
+// Returns the document open in the currently active editor.
+// Returns null if there is no active editor or if the document is not VeMod-supported.
+function getActiveVemodDocument() {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
         vscode.window.showErrorMessage("VeMod: No active document");
         return null;
     }
 
-    const document = activeEditor.document;
+    const document = editor.document;
     if (!vemodLanguageIds.includes(document.languageId)) {
         vscode.window.showErrorMessage("VeMod: Active document is not VeMod assembly or Blue source");
         return null;
     }
 
-    return document.fileName;
+    return document;
 }
 
 async function runVemodOnActiveDocument(extraArgs: string) {
 
-    // TODO: window.showQuickPick for JIT options
-    const activeEditor = vscode.window.activeTextEditor;
-    if (!activeEditor) return;
-
-    const filename = getActiveFilename();
-    if (!filename) {
+    const document = getActiveVemodDocument();
+    if (!document) {
         return;
     }
 
-    const args = ["clear && vemod", filename, extraArgs].join(" ");
+    const args = [`clear && ${vemodCommand}`, document.fileName, extraArgs].join(" ");
 
-    const saveStatus = activeEditor.document.save();
-    if (!saveStatus) {
-        vscode.window.showErrorMessage("VeMod: Failed to save document " + filename);
-        return;
+    if (document.isDirty) {
+    const saveStatus = document.save();
+        if (!saveStatus) {
+            vscode.window.showErrorMessage("VeMod: Failed to save document " + document.fileName);
+            return;
+        }
     }
 
     const terminal = getVemodTerminal();
     terminal.show(false);
     terminal.sendText(args, true);
+}
+
+function stripExtension(path: string) {
+    return path.replace(/\.[^/.]+$/, "");
+}
+
+async function transpileFile() {
+
+    const document = getActiveVemodDocument();
+    if (!document) {
+        return;
+    }
+
+    const args = [ "--transpile", "--no-color", document.fileName ];
+    const child = child_process.spawn(vemodCommand, args);
+
+    child.on('error', err => vscode.window.showErrorMessage(`VeMod: Unable to run command ${vemodCommand}: ${err.toString()}`));
+
+    child.on('exit', (code, signal) => {
+        if (code != null) {
+            if (code > 0) {
+
+                // Failure
+                // Display stderr in VeMod terminal
+                //
+                vscode.window.showErrorMessage(`VeMod: ${vemodCommand} exited with status ${code}`);
+                child.stderr.setEncoding('utf8');
+
+                outputChannel.clear();
+
+                let chunk;
+                while (null !== (chunk = child.stderr.read())) {
+                    outputChannel.append(chunk);
+                }
+
+                outputChannel.show(false);
+
+            } else {
+
+                // Success
+                // Open transpiled document in split editor
+                //
+                const outputPath = `${stripExtension(document.fileName)}.vmd`;
+                vscode.workspace.openTextDocument(outputPath).then(result =>
+                   vscode.window.showTextDocument(result, vscode.ViewColumn.Beside, false)
+                );
+            }
+        } else {
+
+            vscode.window.showErrorMessage(`VeMod: ${vemodCommand} received signal ${signal}`);
+        }
+    });
 }
 
 export async function deactivate() {
